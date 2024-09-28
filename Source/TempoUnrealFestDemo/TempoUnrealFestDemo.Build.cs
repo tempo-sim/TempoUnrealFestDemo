9786f805e846821268befe92fda277528bcab1dd
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-09-27 23:09:42 -0400

    Add Scroll to Zoom, Improve Camera Swing Arms

--- Source/TempoUnrealFestDemo/TempoUnrealFestDemo.Build.cs
+++ Source/TempoUnrealFestDemo/TempoUnrealFestDemo.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class TempoUnrealFestDemo : ModuleRules
 {
@@ -18,7 +19,18 @@ public class TempoUnrealFestDemo : ModuleRules
 			"EnhancedInput",
 			// Tempo
 			"TempoCore",
+			"TempoSensors",
+			"TempoSensorsShared",
+			"TempoCamera",
+			"TempoGeographic",
 		});
+
+        // This path is incorrect for non-Windows platforms in GeoReferencing.Build.cs. Copy it correctly here.
+        if (Target.Platform != UnrealTargetPlatform.Win64)
+        {
+            string GeoReferencingPluginPath = Path.Combine(EngineDirectory, "Plugins/Runtime/GeoReferencing");
+            RuntimeDependencies.Add(Path.Combine(GeoReferencingPluginPath, "Resources/PROJ/*"), StagedFileType.UFS);
+        }
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 	}
