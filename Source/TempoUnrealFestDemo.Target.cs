2aef563d2acb2e3d150e501c3a54ae37209f8123
Peter Melick <pete@temposimulation.com>
2024-09-28 13:50:27 -0400

    Ignore Duplicate Symbols in Windows Build

--- Source/TempoUnrealFestDemo.Target.cs
+++ Source/TempoUnrealFestDemo.Target.cs
@@ -12,17 +12,20 @@ public class TempoUnrealFestDemoTarget : TargetRules
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("TempoUnrealFestDemo");
 
-                if (Platform == UnrealTargetPlatform.Win64)
-                {
-                        ToolChainName = "TempoVCToolChain";
-                }
-                else if (Platform == UnrealTargetPlatform.Mac)
-                {
-                        ToolChainName = "TempoMacToolChain";
-                }
-                else if (Platform == UnrealTargetPlatform.Linux)
-                {
-                        ToolChainName = "TempoLinuxToolChain";
-                }
+        if (Platform == UnrealTargetPlatform.Win64)
+        {
+		// Not sure why this is necessary on Windows in this project. It isn't in our other projects.
+		AdditionalLinkerArguments = "/FORCE:MULTIPLE";
+		bOverrideBuildEnvironment = true;
+                ToolChainName = "TempoVCToolChain";
+        }
+        else if (Platform == UnrealTargetPlatform.Mac)
+        {
+                ToolChainName = "TempoMacToolChain";
+        }
+        else if (Platform == UnrealTargetPlatform.Linux)
+        {
+                ToolChainName = "TempoLinuxToolChain";
+        }
 	}
 }
