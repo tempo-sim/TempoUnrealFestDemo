c50eda31cf226fda9d0505dff1b8485e83136ad5
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-03-15 09:15:57 -0400

    Revive Unreal Fest Demo With Latest Tempo (#11)

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -5,36 +5,6 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT=$(realpath "$SCRIPT_DIR/..")
 cd $PROJECT_ROOT
-PROJECT_NAME=$(find . -maxdepth 1 -name "*.uproject" -exec basename {} .uproject \;)
 
-# Check for UNREAL_ENGINE_PATH
-if [ -z ${UNREAL_ENGINE_PATH+x} ]; then
-  echo "Please set UNREAL_ENGINE_PATH environment variable and re-run";
-  exit 1
-fi
-
-PLATFORM=""
-if [[ "$OSTYPE" = "msys" ]]; then
-  PLATFORM="Win64"
-elif [[ "$OSTYPE" = "darwin"* ]]; then
-  PLATFORM="Mac"
-elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
-  PLATFORM="Linux"
-else
-  echo "Unsupported platform"
-  exit 1
-fi
-
-cd "$UNREAL_ENGINE_PATH"
-if [ "$PLATFORM" = "Win64" ]; then
-  ./Engine/Binaries/Win64/UnrealEditor-Cmd.exe "$PROJECT_ROOT/$PROJECT_NAME.uproject"
-elif [ "$PLATFORM" = "Mac" ]; then
-  ./Engine/Binaries/Mac/UnrealEditor.app/Contents/MacOS/UnrealEditor "$PROJECT_ROOT/$PROJECT_NAME.uproject"
-elif [ "$PLATFORM" = "Linux" ]; then
-  # -norelativemousemode is a workaround for an issue where the mouse is extremely sensitive with Ubuntu remote desktop
-  # https://forums.unrealengine.com/t/work-from-home-how-to-use-unreal-through-remote-desktop/141157
-  ./Engine/Binaries/Linux/UnrealEditor "$PROJECT_ROOT/$PROJECT_NAME.uproject" -norelativemousemode
-else
-  echo "Unsupported platform"
-  exit 1
-fi
+# Simply call the Tempo Run script with the same arguments.
+./Plugins/Tempo/Scripts/Run.sh "$@"
